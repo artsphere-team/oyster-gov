d6f23883f7af2dd70e625ab9bf629214d6271250
Sebastian.Bor <sebastian_bor@hotmail.com>
2021-07-09 20:16:03 +0100

    Updates

---
+++ precache-manifest.3526f54f76567308bed962a8da5f124c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd514ee991747e92e65684896006bc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7f20f377e161f2de592",
+    "url": "./static/css/main.8365f627.chunk.css"
+  },
+  {
+    "revision": "092d4c0814020115fcdc",
+    "url": "./static/js/2.f89721c9.chunk.js"
+  },
+  {
+    "revision": "6653c3b2e097944ee75e4a2ebe220d64",
+    "url": "./static/js/2.f89721c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f20f377e161f2de592",
+    "url": "./static/js/main.ca19b4a7.chunk.js"
+  },
+  {
+    "revision": "6e8b7ceada4c8a2d693c",
+    "url": "./static/js/runtime-main.4a3ee0af.js"
+  },
+  {
+    "revision": "4abc2aec9d00d77e4f7bbd30e88f4e2c",
+    "url": "./static/media/dark-horizontal-combined-rainbow.inline.4abc2aec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2c106016d7b2a898aef44513a0c5fd48.js"
+  "./precache-manifest.3526f54f76567308bed962a8da5f124c.js"
 );
 
 self.addEventListener('message', (event) => {
